00625f890947daf407798b443ac2bcac2b9f43bf
TPD <mig.mic@gmail.com>
2021-02-02 11:38:48 +0100

    test

--- ses1/classA/hello.py
+++ ses1/classA/hello.py
@@ -1,4 +1,13 @@
-def main():
-    return 'Hello'
+import sys
 
-main()
+def main(name):
+    print("hej " + name)
+    a = "vi tester nu"
+    b = len(a)
+    print(a)
+    print(b)
+    a = a + " og nu stopper vi"
+    print(a)
+    print(f'{b} +{a}')
+
+main(sys.argv[1])
